0f382c30a25aa92bb215eb8d2b7754aebd7fae34
TusharNamdev <09tusharnamdev@gmail.com>
2025-06-14 01:52:09 +0530

    Folder

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import HeroSection from "./Components/HeroSection";
+
+const App = () => {
+
+  return (
+    <>
+      <HeroSection/>
+    </>
+  );
+};
+
+export default App;
